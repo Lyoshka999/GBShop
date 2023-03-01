0d4f3c3fdd6d8d533f799aa1adf2f093b564ada7
Lyoshka <alex.sidorenko@mail.ru>
2023-03-01 21:38:54 +0300

    add screen auth

---
+++ GBShop/App/BusinesLogic/Network/Basket/BasketFunction.swift
@@ -0,0 +1,14 @@
+//
+//  BasketFunction.swift
+//  GBShop
+//
+//  Created by Алексей on 01.03.2023.
+//
+
+import Foundation
+
+enum BasketFunction: String {
+    case addToBasket
+    case deleteFromBasket
+    case payBasket
+}
